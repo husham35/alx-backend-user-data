1e2f7d0d8c44034fb4ea1672f60b87998c218392
husham35 <husham35@gmail.com>
2024-09-03 21:49:01 +0000

    more fixes to api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Basic authentication module for the API.
 """
+import re
 import base64
 import binascii
+from typing import Tuple, TypeVar
+
 from .auth import Auth
 from models.user import User
-import re
-from typing import Tuple, TypeVar
 
 
 class BasicAuth(Auth):
