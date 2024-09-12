df20ce872f0d7a0099b622755ec42e05c9c60b17
husham35 <husham35@gmail.com>
2024-09-12 15:14:11 +0000

    task 0 fixes

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """
 User Model
 """
-from sqlalchemy import Column, Integer, String 
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
